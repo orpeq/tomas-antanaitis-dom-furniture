c42da2c38cdb07b324460fc6143453e7c10564e4
Tomas Antanaitis <to.antanaitis@gmail.com>
2021-11-15 12:17:14 +0200

    8: Įgyvendintas komponento atvaizdavimas

--- js/components/FurnitureCardComponent.js
+++ js/components/FurnitureCardComponent.js
@@ -6,10 +6,42 @@ class FurnitureCardComponent{
     }
 
     init = () => {
+        const { title, type, price, location: {country, city, street}, owner: {fullname, mobile, address, email}, imgSrc} = this.props
         this.htmlElement = document.createElement('article');
         this.htmlElement.className = 'card shadow';
         this.htmlElement.innerHTML = `
-        <h2 class="h5">Kortelė</h2>
+        <img src="${imgSrc}" class="card-img-top" / height="300px" style="object-fit: cover"/>
+        <div class="card-body"> 
+        <h2 class="h5">${title}</h2>
+        
+        <ul>
+        <h6><strong>Tipas:</strong>${type}</h6>
+        <li style="list-style:none">
+        <h6><strong>Kaina:</strong>${price} EUR</h6>
+        </li>
+
+        <li style="list-style:none">
+        <h6><strong>Vieta:</strong>  ${street}, ${city}, ${country}</h6>
+        </li>
+
+        <li style="list-style:none">
+        <h6><strong>Pardavėjas:</strong>  ${fullname}</h6>
+        </li>
+       
+        <li style="list-style:none">
+        <h6><strong>Tel. nr.</strong>  ${mobile}</h6>
+        </li>
+
+        <li style="list-style:none">
+        <h6><strong>El. paštas</strong>  ${email}</h6>
+        </li>
+        
+        <li style="list-style:none">
+        <h6><strong>Rajonas</strong>  ${address}</h6>
+        </li>
+
+        </ul>
+        </div>
         `
     }
 }
