532797ba146ec5b59dc1562b78ad7195416055a3
Tomas Antanaitis <to.antanaitis@gmail.com>
2021-11-15 10:47:35 +0200

    6: sukurtas fetch metodas komponente

--- js/components/FurnitureGridComponent.js
+++ js/components/FurnitureGridComponent.js
@@ -1,10 +1,34 @@
 class FurnitureGridComponent {
     constructor() {
+        this.state = {
+            loading: false,
+            furniture: []
+        }
         this.init();
     }
 
+    fetchFurniture = () => API.fetchFurniture(this.saveFurniture, alert);
+
+    saveFurniture = (furniture) => {
+        this.state.furniture = furniture;
+        this.state.loading = false;
+
+        this.render();
+    }
+
     init = () => {
+        this.state.loading = true;
+        this.fetchFurniture();
         this.htmlElement = document.createElement('div');
-        this.htmlElement.innerHTML = 'Furnitūros komponentas';
+        this.render();
+    }
+
+    render = () => {
+        const {loading, furniture} = this.state;
+        if(loading) {
+            this.htmlElement.innerHTML = 'Siunčiama...';
+        } else {
+            this.htmlElement.innerHTML = 'Parsiųsta!';
+        }
     }
 }
